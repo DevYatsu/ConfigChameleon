01bdde15c55bf0168a7df471be4c8e58a53ef543
Yatsu <yatsu.dev@gmail.com>
2023-08-30 05:27:04 +0200

    add a script to generate an import map file on server reload

--- dev.ts
+++ dev.ts
@@ -5,5 +5,7 @@ import "https://deno.land/x/twindellisense@v1.0.0/load.ts";
 
 import dev from "$fresh/dev.ts";
 import config from "./fresh.config.ts";
+import initImportMap from "./map_plugin.ts";
 
+await initImportMap();
 await dev(import.meta.url, "./main.ts", config);
