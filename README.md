95a3cb6ff5f41bcec7a9a40c9f08feefafa5bd30
Yatsu <yatsu.dev@gmail.com>
2023-08-31 01:57:03 +0200

    code update

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 A simple application to convert csv to json and vice-versa created using Deno
 and its Fresh web framework The main purpose of this app is simply to try Deno
 and see if i could easily use it in future projects.
+
+## ToDo
+
+- Add csv support
+- Add a copy button on /password page
+- Add a reload button on /json page
+- Add features to minify files
--- routes/json.tsx
+++ routes/json.tsx
@@ -4,27 +4,23 @@ import { asset, Head, useCSP } from "$fresh/runtime.ts";
 import { Handlers } from "$fresh/server.ts";
 import JsonViewer from "../components/JsonViewer.tsx";
 
-type Todo = {
-  "userId": number;
-  "id": number;
-  "title": string;
-  "completed": boolean;
-};
-
-export const handler: Handlers<Todo> = {
+export const handler: Handlers<[] | object> = {
   async GET(_req, ctx) {
-    const data: Todo = await fetch(
-      `https://jsonplaceholder.typicode.com/todos/${
-        Math.floor(Math.random() * 100)
-      }`,
-    )
-      .then((response) => response.json());
+    try {
+      const data = await fetch(
+        `https://random-data-api.com/api/v2/banks?size=1&response_type=json`,
+      )
+        .then((response) => response.json());
 
-    if (!data) {
-      return new Response("Could not retrieve json data.", { status: 404 });
+      if (!data) {
+        return new Response("Could not retrieve json data.", { status: 404 });
+      }
+      return ctx.render(data);
+    } catch (error) {
+      return ctx.render({
+        error: [[[[[[[[[[[["Failed to fetch data"]]]]]]]]]]]],
+      });
     }
-
-    return ctx.render(data);
   },
 };
 
@@ -40,10 +36,29 @@ export default function ({ data }: PageProps) {
   return (
     <>
       <Head>
+        <title>Generate Random Json</title>
+        <meta
+          property="og:title"
+          content="Generate Random Json"
+        />
+        <meta
+          name="description"
+          content="Generate random json data on reload!"
+        />
+        <meta
+          property="og:description"
+          content="Generate random json data on reload!"
+        />
         <link
           rel="stylesheet"
           href={asset(
             "https://fonts.googleapis.com/css2?family=Pacifico:wght@400&display=swap",
+          )}
+        />
+        <link
+          rel="stylesheet"
+          href={asset(
+            "https://fonts.googleapis.com/css2?family=Sedgwick+Ave+Display:wght@400&display=swap",
           )}
         />
       </Head>
@@ -54,9 +69,12 @@ export default function ({ data }: PageProps) {
             <h1 class="text-slate-900 dark:text-slate-50 font-extrabold font-pacifico text-4xl sm:text-4xl lg:text-5xl tracking-widest z-10 text-center xl:text-left relative px-5 pt-12 xl:pl-40">
               Generate random json
             </h1>
+            <span class="font-secondary text-2xl text-center block sm:absolute -bottom-4 left-1/2 xl:left-48 text-gray-400 select-none -z-10">
+              Reload to try it!
+            </span>
           </div>
 
-          <div class="w-full h-full flex items-center justify-center pt-10 pb-10 sm:pt-32">
+          <div class="w-full h-full flex items-center justify-center pt-12 pb-10">
             <JsonViewer data={data} />
           </div>
         </main>
