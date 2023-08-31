aa0ce70b89a0f0bb1361f0687e9e04efaf1ff4d3
Yatsu <yatsu.dev@gmail.com>
2023-08-31 02:57:36 +0200

    fresh_import_map plugin add

--- dev.ts
+++ dev.ts
@@ -1,11 +1,8 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
-import "https://deno.land/x/twindellisense@v1.0.0/load.ts";
 
-// for adding intellisense through tailwind intellisense plugin
-
+import "$modules/twindellisense@v1.0.0/load.ts";
+import "$modules/fresh_import_map@v1.0.0/load.ts";
 import dev from "$fresh/dev.ts";
 import config from "./fresh.config.ts";
-import initImportMap from "./map_plugin.ts";
 
-await initImportMap();
 await dev(import.meta.url, "./main.ts", config);
